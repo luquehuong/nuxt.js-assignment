47bd159f19a9ce1cb9e5087dddc3d4caf3beb520
Quế Hương <luquehuong@gmail.com>
2018-09-25 17:15:39 +0700

    init

---
+++ components/BeerList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div
+      v-for="item in beers"
+      :key="item.id"
+    >
+      <nuxt-link
+        :to="`/${item.id}`" 
+      >
+        {{ item.name }}
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      beers: []
+    }
+  },
+  async created() {
+    try {
+      const { data } = await axios.get('https://api.punkapi.com/v2/beers')
+      console.log(data)
+      this.beers = data
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ pages/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    detail
+    {{ beer_id }}
+
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData(ctx) {
+    return {
+      beer_id: ctx.params.id
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
